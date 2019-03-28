335b7482455725b5763d9b58403cb64cd7389181
Andrew Schwartz <andrew@aschwartz.me>
2019-03-28 00:01:50 -0600

    add textarea and submit button to accept user inputted URLs

--- src/App.js
+++ src/App.js
@@ -26,10 +26,51 @@ class App extends Component {
       <div className="App" id="card-container">
         <Setting label="Show word vectors in cards?" defaultChecked={false} onChecked={handleWordVectors} name="showWordVectors" />
         <hr />
+        <SourceURLsPrompt />
         <Card wordVectors={wordVectors} />
         <Card wordVectors={wordVectors} />
       </div>
     );
+  }
+}
+
+class SourceURLsPrompt extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '' // textarea initial value
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    let urls = this.state.value.split('\n');
+    console.log(urls);
+    // todo call Mercury API here (async)
+    // todo implement a maximum number of URLs allowed per request
+  }
+
+  render() {
+    const textareaPlaceholder = "Enter your source URLs (one per line)";
+    return (
+      <form onSubmit={this.handleSubmit} className="source-urls-container">
+          <textarea
+            className="source-urls-prompt"
+            rows={6}
+            placeholder={textareaPlaceholder}
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        <input type="submit" value="Get Source Info" />
+      </form>
+    )
   }
 }
 
