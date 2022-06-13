e617f563d2a2e47cb69b327f89ce03b03f4543b6
Andrew Schwartz <andrew@aschwartz.me>
2022-06-12 22:52:48 -0700

    chore: file naming

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import './Header.css';
 
-
 class Header extends Component {
   render() {
     return (
       <div className="header">
-        <h1>cards.cx</h1>
+        <h1>gwhs.cx</h1>
         <h3>Easily transform URLs into debate cards</h3>
         <p>Try it out! Enter URLs into the box below:</p>
       </div>
