a0cb49f3dfa6e64560ea6a2d906188c417c302b4
Andrew Schwartz <andrew@aschwartz.me>
2022-06-12 23:28:56 -0700

    chore: format

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,16 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Header.css';
 
-class Header extends Component {
-  render() {
-    return (
-      <div className="header">
-        <h1>gwhs.cx</h1>
-        <h3>Easily transform URLs into debate cards</h3>
-        <p>Try it out! Enter URLs into the box below:</p>
-      </div>
-    );
-  } // todo style this nicely
-}
+const Header = () => (
+  <div className="header">
+    <h1>gwhs.cx</h1>
+    <h3>Easily transform URLs into debate cards</h3>
+    <p>Try it out! Enter URLs into the box below:</p>
+  </div>
+);
 
 export default Header;
